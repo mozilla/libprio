ded8b1054b84012676b28772fd7594476652752a
Robert Helmer <robert@roberthelmer.com>
2018-08-29 13:47:18 -0700

    issue #25 - format libprio with clang-format, using Mozilla style (#26)
    * issue #25 - format libprio with clang-format, using Mozilla style
    
    * force clang 3.9 (latest available on Ubuntu Trusty)
    
    * remove clang-format file, 3.9 is confused by some settings. try using built-in Mozilla style instead

--- pclient/main.c
+++ pclient/main.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018, Henry Corrigan-Gibbs
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,7 +13,7 @@
 #include "prio/util.h"
 
 int
-verify_full (void)
+verify_full(void)
 {
   SECStatus rv = SECSuccess;
 
@@ -34,14 +34,14 @@ verify_full (void)
   PrioTotalShare tA = NULL;
   PrioTotalShare tB = NULL;
 
-  unsigned char *for_server_a = NULL;
-  unsigned char *for_server_b = NULL;
+  unsigned char* for_server_a = NULL;
+  unsigned char* for_server_b = NULL;
 
-  const unsigned char *batch_id = (unsigned char *)"prio_batch_2018-04-17";
-  const unsigned int batch_id_len = strlen ((char *)batch_id);
+  const unsigned char* batch_id = (unsigned char*)"prio_batch_2018-04-17";
+  const unsigned int batch_id_len = strlen((char*)batch_id);
 
   // Initialize NSS random number generator.
-  P_CHECKC (Prio_init ());
+  P_CHECKC(Prio_init());
 
   // Number of different boolean data fields we collect.
   const int ndata = 100;
@@ -49,50 +49,49 @@ verify_full (void)
   // Number of clients to simulate.
   const int nclients = 10;
 
-
   // New scope to avoid goto weirdness
   {
     bool data_items[ndata];
 
     // Generate keypairs for servers
-    P_CHECKC (Keypair_new (&skA, &pkA));
-    P_CHECKC (Keypair_new (&skB, &pkB));
+    P_CHECKC(Keypair_new(&skA, &pkA));
+    P_CHECKC(Keypair_new(&skB, &pkB));
 
     // Use the default configuration parameters.
-    P_CHECKA (cfg = PrioConfig_new (ndata, pkA, pkB, batch_id, batch_id_len));
+    P_CHECKA(cfg = PrioConfig_new(ndata, pkA, pkB, batch_id, batch_id_len));
 
     PrioPRGSeed server_secret;
-    P_CHECKC (PrioPRGSeed_randomize (&server_secret));
+    P_CHECKC(PrioPRGSeed_randomize(&server_secret));
 
     // Initialize two server objects. The role of the servers need not
     // be symmetric. In a deployment, we envision that:
-    //   * Server A is the main telemetry server that is always online. 
+    //   * Server A is the main telemetry server that is always online.
     //     Clients send their encrypted data packets to Server A and
     //     Server A stores them.
     //   * Server B only comes online when the two servers want to compute
     //     the final aggregate statistics.
-    P_CHECKA (sA = PrioServer_new (cfg, PRIO_SERVER_A, skA, server_secret));
-    P_CHECKA (sB = PrioServer_new (cfg, PRIO_SERVER_B, skB, server_secret));
+    P_CHECKA(sA = PrioServer_new(cfg, PRIO_SERVER_A, skA, server_secret));
+    P_CHECKA(sB = PrioServer_new(cfg, PRIO_SERVER_B, skB, server_secret));
 
     // Initialize empty verifier objects
-    P_CHECKA (vA = PrioVerifier_new (sA)); 
-    P_CHECKA (vB = PrioVerifier_new (sB));
+    P_CHECKA(vA = PrioVerifier_new(sA));
+    P_CHECKA(vB = PrioVerifier_new(sB));
 
     // Initialize shares of final aggregate statistics
-    P_CHECKA (tA = PrioTotalShare_new ());
-    P_CHECKA (tB = PrioTotalShare_new ());
+    P_CHECKA(tA = PrioTotalShare_new());
+    P_CHECKA(tB = PrioTotalShare_new());
 
     // Initialize shares of verification packets
-    P_CHECKA (p1A = PrioPacketVerify1_new ());
-    P_CHECKA (p1B = PrioPacketVerify1_new ());
-    P_CHECKA (p2A = PrioPacketVerify2_new ());
-    P_CHECKA (p2B = PrioPacketVerify2_new ());
+    P_CHECKA(p1A = PrioPacketVerify1_new());
+    P_CHECKA(p1B = PrioPacketVerify1_new());
+    P_CHECKA(p2A = PrioPacketVerify2_new());
+    P_CHECKA(p2B = PrioPacketVerify2_new());
 
     // Generate client data packets.
-    for (int c=0; c < nclients; c++) {
+    for (int c = 0; c < nclients; c++) {
 
       // The client's data submission is an arbitrary boolean vector.
-      for (int i=0; i < ndata; i++) {
+      for (int i = 0; i < ndata; i++) {
         // Arbitrary data
         data_items[i] = (i % 3 == 1) || (c % 5 == 3);
       }
@@ -101,25 +100,24 @@ verify_full (void)
       //
       // Construct the client data packets.
       unsigned int aLen, bLen;
-      P_CHECKC (PrioClient_encode (cfg, data_items, 
-            &for_server_a, &aLen, &for_server_b, &bLen));
+      P_CHECKC(PrioClient_encode(cfg, data_items, &for_server_a, &aLen,
+                                 &for_server_b, &bLen));
 
       // The Prio servers A and B can come online later (e.g., at the end of
       // each day) to download the encrypted telemetry packets from the
       // telemetry server and run the protocol that computes the aggregate
-      // statistics. In this way, the client only needs to send a 
-      // single message (the pair of encrypted ClientPacketData packets) 
+      // statistics. In this way, the client only needs to send a
+      // single message (the pair of encrypted ClientPacketData packets)
       // to a single server (the telemetry-data-collection server).
 
       // THE CLIENT'S JOB IS DONE. The rest of the processing just takes place
       // between the two servers A and B.
 
-
       // II. VALIDATION PROTOCOL. (at servers)
       //
-      // The servers now run a short 2-step protocol to check each 
+      // The servers now run a short 2-step protocol to check each
       // client's packet:
-      //    1) Servers A and B broadcast one message (PrioPacketVerify1) 
+      //    1) Servers A and B broadcast one message (PrioPacketVerify1)
       //       to each other.
       //    2) Servers A and B broadcast another message (PrioPacketVerify2)
       //       to each other.
@@ -128,37 +126,37 @@ verify_full (void)
       //       running total of aggregate statistics) or ill-formed
       //       (in which case they ignore it).
       // These messages must be sent over an authenticated channel, so
-      // that each server is assured that every received message came 
+      // that each server is assured that every received message came
       // from its peer.
 
       // Set up a Prio verifier object.
-      P_CHECKC (PrioVerifier_set_data (vA, for_server_a, aLen));
-      P_CHECKC (PrioVerifier_set_data (vB, for_server_b, bLen));
+      P_CHECKC(PrioVerifier_set_data(vA, for_server_a, aLen));
+      P_CHECKC(PrioVerifier_set_data(vB, for_server_b, bLen));
 
       // Both servers produce a packet1. Server A sends p1A to Server B
       // and vice versa.
-      P_CHECKC (PrioPacketVerify1_set_data (p1A, vA));
-      P_CHECKC (PrioPacketVerify1_set_data (p1B, vB));
+      P_CHECKC(PrioPacketVerify1_set_data(p1A, vA));
+      P_CHECKC(PrioPacketVerify1_set_data(p1B, vB));
 
       // Both servers produce a packet2. Server A sends p2A to Server B
       // and vice versa.
-      P_CHECKC (PrioPacketVerify2_set_data(p2A, vA, p1A, p1B));
-      P_CHECKC (PrioPacketVerify2_set_data(p2B, vB, p1A, p1B));
+      P_CHECKC(PrioPacketVerify2_set_data(p2A, vA, p1A, p1B));
+      P_CHECKC(PrioPacketVerify2_set_data(p2B, vB, p1A, p1B));
 
       // Using p2A and p2B, the servers can determine whether the request
-      // is valid. (In fact, only Server A needs to perform this 
-      // check, since Server A can just tell Server B whether the check 
-      // succeeded or failed.) 
-      P_CHECKC (PrioVerifier_isValid (vA, p2A, p2B)); 
-      P_CHECKC (PrioVerifier_isValid (vB, p2A, p2B)); 
+      // is valid. (In fact, only Server A needs to perform this
+      // check, since Server A can just tell Server B whether the check
+      // succeeded or failed.)
+      P_CHECKC(PrioVerifier_isValid(vA, p2A, p2B));
+      P_CHECKC(PrioVerifier_isValid(vB, p2A, p2B));
 
       // If we get here, the client packet is valid, so add it to the aggregate
       // statistic counter for both servers.
-      P_CHECKC (PrioServer_aggregate (sA, vA));
-      P_CHECKC (PrioServer_aggregate (sB, vB));
+      P_CHECKC(PrioServer_aggregate(sA, vA));
+      P_CHECKC(PrioServer_aggregate(sB, vB));
 
-      free (for_server_a);
-      free (for_server_b);
+      free(for_server_a);
+      free(for_server_b);
       for_server_a = NULL;
       for_server_b = NULL;
     }
@@ -168,62 +166,63 @@ verify_full (void)
     // III. PRODUCTION OF AGGREGATE STATISTICS.
     //
     // After collecting aggregates from MANY clients, the servers can compute
-    // their shares of the aggregate statistics. 
+    // their shares of the aggregate statistics.
     //
     // Server B can send tB to Server A.
-    P_CHECKC (PrioTotalShare_set_data (tA, sA));
-    P_CHECKC (PrioTotalShare_set_data (tB, sB));
+    P_CHECKC(PrioTotalShare_set_data(tA, sA));
+    P_CHECKC(PrioTotalShare_set_data(tB, sB));
 
     // Once Server A has tA and tB, it can learn the aggregate statistics
     // in the clear.
     unsigned long output[ndata];
-    P_CHECKC (PrioTotalShare_final (cfg, output, tA, tB));
-    
+    P_CHECKC(PrioTotalShare_final(cfg, output, tA, tB));
+
     // Now the output[i] contains a counter that indicates how many clients
     // submitted TRUE for data value i.  We print out this data.
-    for (int i=0; i < ndata; i++) 
+    for (int i = 0; i < ndata; i++)
       printf("output[%d] = %lu\n", i, output[i]);
   }
 
 cleanup:
   if (rv != SECSuccess) {
-    fprintf (stderr, "Warning: unexpected failure.\n");
+    fprintf(stderr, "Warning: unexpected failure.\n");
   }
 
-  if (for_server_a) free (for_server_a);
-  if (for_server_b) free (for_server_b);
+  if (for_server_a)
+    free(for_server_a);
+  if (for_server_b)
+    free(for_server_b);
 
-  PrioTotalShare_clear (tA);
-  PrioTotalShare_clear (tB);
+  PrioTotalShare_clear(tA);
+  PrioTotalShare_clear(tB);
 
-  PrioPacketVerify2_clear (p2A);
-  PrioPacketVerify2_clear (p2B);
+  PrioPacketVerify2_clear(p2A);
+  PrioPacketVerify2_clear(p2B);
 
-  PrioPacketVerify1_clear (p1A);
-  PrioPacketVerify1_clear (p1B);
+  PrioPacketVerify1_clear(p1A);
+  PrioPacketVerify1_clear(p1B);
 
-  PrioVerifier_clear (vA);
-  PrioVerifier_clear (vB);
+  PrioVerifier_clear(vA);
+  PrioVerifier_clear(vB);
 
-  PrioServer_clear (sA);
-  PrioServer_clear (sB);
-  PrioConfig_clear (cfg);
+  PrioServer_clear(sA);
+  PrioServer_clear(sB);
+  PrioConfig_clear(cfg);
 
-  PublicKey_clear (pkA);
-  PublicKey_clear (pkB);
+  PublicKey_clear(pkA);
+  PublicKey_clear(pkB);
 
-  PrivateKey_clear (skA);
-  PrivateKey_clear (skB);
+  PrivateKey_clear(skA);
+  PrivateKey_clear(skB);
 
-  Prio_clear ();
+  Prio_clear();
 
   return !(rv == SECSuccess);
 }
 
-int 
-main (void)
+int
+main(void)
 {
-  puts ("This utility demonstrates how to invoke the Prio API.");
-  return verify_full ();
+  puts("This utility demonstrates how to invoke the Prio API.");
+  return verify_full();
 }
-
--- ptest/fft_test.c
+++ ptest/fft_test.c
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2018, Henry Corrigan-Gibbs
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 #include <mpi.h>
@@ -16,59 +16,59 @@
 #include "prio/poly.h"
 #include "prio/util.h"
 
-void 
-mu_test__fft_one (void)
+void
+mu_test__fft_one(void)
 {
   SECStatus rv = SECSuccess;
   PrioConfig cfg = NULL;
   MPArray points_in = NULL;
   MPArray points_out = NULL;
 
-  P_CHECKA (cfg = PrioConfig_newTest (123));
-  P_CHECKA (points_in = MPArray_new (1));
-  P_CHECKA (points_out = MPArray_new (1));
+  P_CHECKA(cfg = PrioConfig_newTest(123));
+  P_CHECKA(points_in = MPArray_new(1));
+  P_CHECKA(points_out = MPArray_new(1));
 
-  mp_set (&points_in->data[0], 3);
-  mu_check (poly_fft (points_out, points_in, cfg, false) == SECSuccess);
+  mp_set(&points_in->data[0], 3);
+  mu_check(poly_fft(points_out, points_in, cfg, false) == SECSuccess);
 
-  mu_check (mp_cmp_d(&points_in->data[0], 3) == 0);
-  mu_check (mp_cmp_d(&points_out->data[0], 3) == 0);
+  mu_check(mp_cmp_d(&points_in->data[0], 3) == 0);
+  mu_check(mp_cmp_d(&points_out->data[0], 3) == 0);
 
 cleanup:
-  mu_check (rv == SECSuccess);
-  MPArray_clear (points_in);
-  MPArray_clear (points_out);
+  mu_check(rv == SECSuccess);
+  MPArray_clear(points_in);
+  MPArray_clear(points_out);
 
-  PrioConfig_clear (cfg);
+  PrioConfig_clear(cfg);
 }
 
-void 
-mu_test__fft_roots (void)
+void
+mu_test__fft_roots(void)
 {
   SECStatus rv = SECSuccess;
   PrioConfig cfg = NULL;
   mp_int tmp;
-  MP_DIGITS (&tmp) = NULL;
+  MP_DIGITS(&tmp) = NULL;
 
-  P_CHECKA (cfg = PrioConfig_newTest (90));
-  MP_CHECKC (mp_init (&tmp));
+  P_CHECKA(cfg = PrioConfig_newTest(90));
+  MP_CHECKC(mp_init(&tmp));
 
   mp_int roots[4];
-  poly_fft_get_roots (roots, 4, cfg, false);
+  poly_fft_get_roots(roots, 4, cfg, false);
 
-  for (int i=0; i<4; i++) {
+  for (int i = 0; i < 4; i++) {
     mp_exptmod_d(&roots[i], 4, &cfg->modulus, &tmp);
-    mu_check (mp_cmp_d( &tmp, 1) == 0);
+    mu_check(mp_cmp_d(&tmp, 1) == 0);
   }
 
 cleanup:
-  mu_check (rv == SECSuccess);
-  mp_clear (&tmp);
-  PrioConfig_clear (cfg);
+  mu_check(rv == SECSuccess);
+  mp_clear(&tmp);
+  PrioConfig_clear(cfg);
 }
 
-void 
-mu_test__fft_simple (void)
+void
+mu_test__fft_simple(void)
 {
   SECStatus rv = SECSuccess;
   const int nPoints = 4;
@@ -79,32 +79,34 @@ mu_test__fft_simple (void)
 
   mp_int should_be, tmp;
   mp_int roots[nPoints];
-  MP_DIGITS (&should_be) = NULL;
-  MP_DIGITS (&tmp) = NULL;
-  for (int i=0; i<nPoints; i++) {
-    MP_DIGITS (&roots[i]) = NULL;
+  MP_DIGITS(&should_be) = NULL;
+  MP_DIGITS(&tmp) = NULL;
+  for (int i = 0; i < nPoints; i++) {
+    MP_DIGITS(&roots[i]) = NULL;
   }
-  
-  P_CHECKA (cfg = PrioConfig_newTest (140));
-  P_CHECKA (points_in = MPArray_new (nPoints));
-  P_CHECKA (points_out = MPArray_new (nPoints));
-  MP_CHECKC (mp_init (&should_be));
-  MP_CHECKC (mp_init (&tmp));
 
-  poly_fft_get_roots (roots, nPoints, cfg, false);
+  P_CHECKA(cfg = PrioConfig_newTest(140));
+  P_CHECKA(points_in = MPArray_new(nPoints));
+  P_CHECKA(points_out = MPArray_new(nPoints));
+  MP_CHECKC(mp_init(&should_be));
+  MP_CHECKC(mp_init(&tmp));
 
-  mp_set (&points_in->data[0], 3);
-  mp_set (&points_in->data[1], 8);
-  mp_set (&points_in->data[2], 7);
-  mp_set (&points_in->data[3], 9);
-  mu_check (poly_fft (points_out, points_in, cfg, false) == SECSuccess);
+  poly_fft_get_roots(roots, nPoints, cfg, false);
 
-  for (int i=0; i<nPoints; i++) {
-    mp_set (&should_be, 0);
-    for (int j=0; j<nPoints; j++) {
-      mu_check (mp_exptmod_d(&roots[i], j, &cfg->modulus, &tmp) == MP_OKAY);
-      mu_check (mp_mulmod(&tmp, &points_in->data[j], &cfg->modulus, &tmp) == MP_OKAY);
-      mu_check (mp_addmod(&should_be, &tmp, &cfg->modulus, &should_be) == MP_OKAY);
+  mp_set(&points_in->data[0], 3);
+  mp_set(&points_in->data[1], 8);
+  mp_set(&points_in->data[2], 7);
+  mp_set(&points_in->data[3], 9);
+  mu_check(poly_fft(points_out, points_in, cfg, false) == SECSuccess);
+
+  for (int i = 0; i < nPoints; i++) {
+    mp_set(&should_be, 0);
+    for (int j = 0; j < nPoints; j++) {
+      mu_check(mp_exptmod_d(&roots[i], j, &cfg->modulus, &tmp) == MP_OKAY);
+      mu_check(mp_mulmod(&tmp, &points_in->data[j], &cfg->modulus, &tmp) ==
+               MP_OKAY);
+      mu_check(mp_addmod(&should_be, &tmp, &cfg->modulus, &should_be) ==
+               MP_OKAY);
     }
 
     /*
@@ -114,20 +116,20 @@ mu_test__fft_simple (void)
     mp_print(&points_out[i], stdout);
     puts("");
     */
-    mu_check (mp_cmp (&should_be, &points_out->data[i]) == 0);
+    mu_check(mp_cmp(&should_be, &points_out->data[i]) == 0);
   }
 
 cleanup:
-  mu_check (rv == SECSuccess);
-  mp_clear (&tmp);
-  mp_clear (&should_be);
-  MPArray_clear (points_in);
-  MPArray_clear (points_out);
-  PrioConfig_clear (cfg);
+  mu_check(rv == SECSuccess);
+  mp_clear(&tmp);
+  mp_clear(&should_be);
+  MPArray_clear(points_in);
+  MPArray_clear(points_out);
+  PrioConfig_clear(cfg);
 }
 
-void 
-mu_test__fft_invert (void)
+void
+mu_test__fft_invert(void)
 {
   SECStatus rv = SECSuccess;
   const int nPoints = 8;
@@ -137,34 +139,34 @@ mu_test__fft_invert (void)
   MPArray points_out = NULL;
   MPArray points_out2 = NULL;
   mp_int roots[nPoints];
-  
-  P_CHECKA (cfg = PrioConfig_newTest (91));
-  P_CHECKA (points_in = MPArray_new (nPoints)); 
-  P_CHECKA (points_out = MPArray_new (nPoints));
-  P_CHECKA (points_out2 = MPArray_new (nPoints));
 
-  poly_fft_get_roots (roots, nPoints, cfg, false);
+  P_CHECKA(cfg = PrioConfig_newTest(91));
+  P_CHECKA(points_in = MPArray_new(nPoints));
+  P_CHECKA(points_out = MPArray_new(nPoints));
+  P_CHECKA(points_out2 = MPArray_new(nPoints));
 
-  mp_set (&points_in->data[0], 3);
-  mp_set (&points_in->data[1], 8);
-  mp_set (&points_in->data[2], 7);
-  mp_set (&points_in->data[3], 9);
-  mp_set (&points_in->data[4], 123);
-  mp_set (&points_in->data[5], 123123987);
-  mp_set (&points_in->data[6], 2);
-  mp_set (&points_in->data[7], 0);
-  mu_check (poly_fft(points_out, points_in, cfg, false) == SECSuccess);
-  mu_check (poly_fft(points_out2, points_out, cfg, true) == SECSuccess);
+  poly_fft_get_roots(roots, nPoints, cfg, false);
 
-  for (int i=0; i<nPoints; i++) {
-    mu_check (mp_cmp (&points_out2->data[i], &points_in->data[i]) == 0);
+  mp_set(&points_in->data[0], 3);
+  mp_set(&points_in->data[1], 8);
+  mp_set(&points_in->data[2], 7);
+  mp_set(&points_in->data[3], 9);
+  mp_set(&points_in->data[4], 123);
+  mp_set(&points_in->data[5], 123123987);
+  mp_set(&points_in->data[6], 2);
+  mp_set(&points_in->data[7], 0);
+  mu_check(poly_fft(points_out, points_in, cfg, false) == SECSuccess);
+  mu_check(poly_fft(points_out2, points_out, cfg, true) == SECSuccess);
+
+  for (int i = 0; i < nPoints; i++) {
+    mu_check(mp_cmp(&points_out2->data[i], &points_in->data[i]) == 0);
   }
 
 cleanup:
-  mu_check (rv == SECSuccess);
+  mu_check(rv == SECSuccess);
 
-  MPArray_clear (points_in);
-  MPArray_clear (points_out);
-  MPArray_clear (points_out2);
-  PrioConfig_clear (cfg);
+  MPArray_clear(points_in);
+  MPArray_clear(points_out);
+  MPArray_clear(points_out2);
+  PrioConfig_clear(cfg);
 }
